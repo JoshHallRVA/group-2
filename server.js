d8915074f1f9af492dc6d55e82212bbd744c21fd
Kendall Masterson <masterson.kendall@gmail.com>
2020-09-01 21:36:43 -0400

    added to server.js for aws

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require("express");
 require("dotenv").config();
 
+
+const aws = require('aws-sdk');
+aws.config.region = 'us-east-2';
+
+
+// app.engine('html', require('ejs').renderFile);
+
+
+const S3_BUCKET = process.env.S3_BUCKET;
+
+
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -11,6 +22,35 @@ app.use(express.static("public"));
 
 // require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes.js")(app);
+
+app.get('/sign-s3', (req, res) => {
+	const s3 = new aws.S3();
+	const fileName = req.query['file-name'];
+	const fileType = req.query['file-type'];
+	const s3Params = {
+	  Bucket: S3_BUCKET,
+	  Key: fileName,
+	  Expires: 60,
+	  ContentType: fileType,
+	  ACL: 'public-read'
+	};
+	s3.getSignedUrl('putObject', s3Params, (err, data) => {
+	  if(err){
+		console.log(err);
+		return res.end();
+	  }
+	  const returnData = {
+		signedRequest: data,
+		url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+	  };
+	  res.write(JSON.stringify(returnData));
+	  res.end();
+	});
+  });
+
+  app.post('/save-details', (req, res) => {
+	console.log(req)
+  });
 
 app.listen(PORT, function () {
 	console.log("App listening on PORT: " + PORT);
