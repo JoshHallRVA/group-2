46a6fd2e1c386bf8b7fdabb5e5b5d00305e68db9
josh hall <HallHammR@gmail.com>
2020-09-03 11:57:46 -0400

    backend working

---
+++ app/data/tableData.js
@@ -0,0 +1,11 @@
+var tableArray = [
+	{
+		customerName: "Ahmed",
+		customerEmail: "ahmed@example.com",
+		customerBrand: "afhaque89",
+		phoneNumber: "000-000-0000",
+	},
+];
+
+//This makes it accessible to other files using require.
+module.exports = tableArray;
