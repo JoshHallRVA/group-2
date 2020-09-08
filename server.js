ed803361f0a820dcec66946320a10121dcf2b66b
Kendall Masterson <masterson.kendall@gmail.com>
2020-09-08 14:27:36 -0400

    s3 bucket works

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const S3_BUCKET = process.env.S3_BUCKET;
 
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
