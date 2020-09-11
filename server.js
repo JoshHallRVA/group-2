7d7157581421e993e8812a5035d08450fd159563
Kendall Masterson <masterson.kendall@gmail.com>
2020-09-11 11:13:34 -0400

    added aws code

--- server.js
+++ server.js
@@ -4,7 +4,11 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
+var bodyParser = require("body-parser");
 var express = require("express");
+var path = require("path");
+require("dotenv").config();
+
 
 // Sets up the Express App
 // =============================================================
@@ -23,34 +27,10 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
+require("./routes/post-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
 
-app.get("/sign-s3", (req, res) => {
-	const s3 = new aws.S3();
-	const fileName = req.query["file-name"];
-	const fileType = req.query["file-type"];
-	const s3Params = {
-		Bucket: S3_BUCKET,
-		Key: fileName,
-		Expires: 60,
-		ContentType: fileType,
-		ACL: "public-read",
-	};
-	s3.getSignedUrl("putObject", s3Params, (err, data) => {
-		if (err) {
-			console.log(err);
-			return res.end();
-		}
-		const returnData = {
-			signedRequest: data,
-			url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
-		};
-		res.write(JSON.stringify(returnData));
-		res.end();
-	});
-});
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
